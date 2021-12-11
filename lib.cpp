cc7993f9a6c2f79f8dee95100939f6e39e973daa
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2021-12-11 03:16:26 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+//
+// Created by Kenneth Haga on 11/12/21.
+//
 
+#include "lib.h"
+float numeroMassimo (const float numeri[], int dim){
+    numeri [dim];
+    float massimo =numeri[0];
+    for (int i=0;i<dim;i++){
+        if(massimo < numeri[i]){
+            massimo = numeri [i];
+        }
+    }
+    return massimo;
+}
