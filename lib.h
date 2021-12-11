12939f52821855cd64a86f6bedd54c926793fef2
S5227686Xavier <91663950+S5227686Xavier@users.noreply.github.com>
2021-12-11 03:15:24 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by Kenneth Haga on 11/12/21.
+//
 
+#ifndef ESERCITAZIONE_8_LIB_H
+#define ESERCITAZIONE_8_LIB_H
+float numeroMassimo (const float numeri[], int dimensione);
+
+#endif //ESERCITAZIONE_8_LIB_H
